f74696ef91e0135b6757671c8406a5e0ffe59ed6
hzk <705248010@qq.com>
2020-11-01 14:39:07 +0800

    ui/info.py change

--- ui/infoUI.py
+++ ui/infoUI.py
@@ -13,12 +13,12 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(401, 215)
+        Form.resize(500, 230)
         self.label_2 = QtWidgets.QLabel(Form)
-        self.label_2.setGeometry(QtCore.QRect(210, 190, 181, 20))
+        self.label_2.setGeometry(QtCore.QRect(250, 190, 181, 20))
         self.label_2.setObjectName("label_2")
         self.label_3 = QtWidgets.QLabel(Form)
-        self.label_3.setGeometry(QtCore.QRect(40, 90, 101, 16))
+        self.label_3.setGeometry(QtCore.QRect(40, 90, 480, 16))
         self.label_3.setObjectName("label_3")
         self.label_4 = QtWidgets.QLabel(Form)
         self.label_4.setGeometry(QtCore.QRect(40, 60, 271, 16))
@@ -34,6 +34,7 @@ class Ui_Form(object):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("Form", "Form"))
         self.label_2.setText(_translate("Form", "All Rights Received @kurong"))
-        self.label_3.setText(_translate("Form", "本项目github地址，http://github.com/705248010/face_detection-with-GUI/tree/master"))
+        self.label_3.setText(
+            _translate("Form", "github: https://github.com/705248010/face_detection-with-GUI/tree/master"))
         self.label_4.setText(_translate("Form", "开发时间：2020.11.1"))
         self.label_5.setText(_translate("Form", "开发者：枯荣止蚀(hzk)"))
